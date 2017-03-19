09c3e05f5aa21b6c28139d3351562d3820074627
zhuzhiqiang <mzhuzhiqiang@163.com>
2017-03-19 22:22:09 +0800

    注释
    添加了一些新的组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public isCollapsed: boolean = false;
   title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AccordionModule } from 'ng2-bootstrap';
+
+// ng-bootstrap
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ng2-bootstrap';
+import { ButtonsModule } from 'ng2-bootstrap';
+import { CarouselModule } from 'ng2-bootstrap';
+import { CollapseModule } from 'ng2-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,9 +19,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AccordionModule.forRoot(),
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
